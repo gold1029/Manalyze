f09c5a30d0dd7131dca24e0d673161486c6162be
JusticeRage <justicerage@manalyzer.org>
2016-11-29 19:35:00 +0100

    Resumed work on the OpenSSL Authenticode verification. The PE digest is now read from the ASN1.

--- plugins/plugin_authenticode_openssl.cpp
+++ plugins/plugin_authenticode_openssl.cpp
@@ -15,19 +15,37 @@
     along with Manalyze.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+#include <string.h>
+#include <sstream>
+
 #include <boost/shared_ptr.hpp>
+#include <boost/cstdint.hpp>
 
 #include <openssl/pkcs7.h>
 #include <openssl/x509.h>
 #include <openssl/bio.h>
+#include <openssl/asn1.h>
 
 #include "plugin_framework/plugin_interface.h"
 
-typedef boost::shared_ptr<PKCS7>  pPKCS7;
-typedef boost::shared_ptr<BIO>    pBIO;
+typedef std::vector<boost::uint8_t> bytes;
+typedef boost::shared_ptr<PKCS7>    pPKCS7;
+typedef boost::shared_ptr<BIO>      pBIO;
+
+const std::string SPC_INDIRECT_DATA = "1.3.6.1.4.1.311.2.1.4";
+const std::string SPC_PE_IMAGE_DATAOBJ = "1.3.6.1.4.1.311.2.1.15";
+const std::string MD5_OID = "1.2.840.113549.2.5";
+const std::string SHA1_OID = "1.3.14.3.2.26";
 
 namespace plugin
 {
+
+// A simple struct describing the digest. The first member is the algorithm used (OID), and the second member is the digest.
+struct AuthenticodeDigest
+{
+    std::string algorithm;
+    bytes digest;
+};
 
 /**
  *  @brief  Returns the contents of an OpenSSL BIO as a string.
@@ -62,6 +80,213 @@ std::string X509_NAME_to_string(X509_NAME* name)
     pBIO bio_out(BIO_new(BIO_s_mem()), BIO_free);
     X509_NAME_print_ex(bio_out.get(), name, 0, 0);
     return bio_to_string(bio_out);
+}
+
+// ----------------------------------------------------------------------------
+
+/**
+ *  @brief  Converts an hexadecimal OID into its string representation.
+ *
+ *  @param  const bytes& in The raw OID bytes.
+ *
+ *  @return A string containing the OID in its (somewhat) human-readable form.
+ */
+std::string OID_to_string(const bytes& in)
+{
+    if (in.size() == 0) {
+        return "";
+    }
+    std::stringstream ss;
+    
+    int b = in[0] % 40;
+    int a = (in[0] - b) / 40;
+    ss << a << "." << b;
+
+    for (unsigned int i = 1 ; i < in.size() ; ++i)
+    {
+        ss << ".";
+        if (in[i] < 128) {
+            ss << static_cast<int>(in[i]); // Do not interpret as a char. 
+        }
+        else 
+        {
+            if (i+1 >= in.size()) // Don't read outside of the bounds. 
+            {
+                    PRINT_WARNING << "[plugin_authenticode] Tried to convert a malformed OID!" << std::endl;
+                    return "";
+            }
+            ss << static_cast<int>((in[i]-128)*128 + in[i+1]);
+            ++i; 
+        }
+    }
+    return ss.str();
+}
+
+// ----------------------------------------------------------------------------
+    
+/**
+ *  @brief  This function asserts that a PKCS7 object has a valid structure
+ *          before attempting any operations on it.
+ *
+ *  @param  pPKCS7 p The PKCS7 object to verify.
+ *
+ *  @return Whether the object can be used safely to verify an Authenticode signature.
+ */
+bool check_pkcs_sanity(pPKCS7 p)
+{
+    if (p == nullptr)
+    {
+        PRINT_WARNING << "[plugin_authenticode] Error reading the PKCS7 certificate." << std::endl;
+        return false;
+    }
+
+    if (!PKCS7_type_is_signed(p.get()))
+    {
+        PRINT_WARNING << "[plugin_authenticode] The PKCS7 structure is not signed!" << std::endl;
+        return false;
+    }
+
+    // The SpcIndirectDataContent structure of the signature cannot be accessed directly
+    // with OpenSSL's API. Retrieve the information manually.
+    if (p->d.sign == nullptr || 
+        p->d.sign->contents == nullptr ||
+        p->d.sign->contents->type == nullptr ||
+        p->d.sign->contents->type->data == nullptr ||
+        p->d.sign->contents->d.other == nullptr ||
+        p->d.sign->contents->d.other->value.asn1_string == nullptr)
+    {
+        PRINT_WARNING << "[plugin_authenticode] Unable to access the "
+                            "SpcIndirectDataContent structure." << std::endl;
+        return false;
+    }
+
+    // Assert that the data indeed points to a SpcIndirectDataContent object by checking the OID.
+    bytes oid(p->d.sign->contents->type->data, 
+                p->d.sign->contents->type->data + p->d.sign->contents->type->length);
+    if (OID_to_string(oid) != SPC_INDIRECT_DATA)
+    {
+        PRINT_WARNING << "[plugin_authenticode] Unable to access the "
+                            "SpcIndirectDataContent structure." << std::endl;
+        return false;
+    }
+
+    return true;
+}
+
+// ----------------------------------------------------------------------------
+
+/**
+ *  @brief  Helper function designed to read ASN1 objects.
+ *
+ *  This function is useful to read objects of an expected type. Its main use is
+ *  to avoid code duplication around error messages.
+ *
+ *  @param  const unsigned char** data A pointer to the ASN1 string to read.
+ *          It will be updated to point to the next object in the string. 
+ *  @param  long max_length The maximum number of bytes to read.
+ *  @param  const std::string& expected The object type expected (i.e. "SEQUENCE").
+ *          This argument is given as a string for code readability.
+ *  @param  const std::string& structure_name The name of the object read (for error messages only).
+ *
+ *  @return The size of the object read. The data pointer will be updated to point to it.
+ */
+long asn1_read(const unsigned char** data,
+               long max_length,
+               const std::string& expected,
+               const std::string& object_name)
+{
+    int tag = 0, xclass = 0;
+    long size = 0;
+
+    ASN1_get_object(data, &size, &tag, &xclass, max_length); // Return value ignored. Who knows what this function returns?
+    std::string tag_s = ASN1_tag2str(tag);
+    if (tag_s != expected)
+    {
+        PRINT_WARNING << "[plugin_authenticode] The " << object_name << " ASN1 string is malformed!" << std::endl;
+        PRINT_WARNING << "(Expected " << expected << ", but got " << tag_s << " instead.)" << std::endl;
+        return 0;
+    }
+    return size;
+}
+
+// ----------------------------------------------------------------------------
+
+/**
+ *  @brief  This function parses an ASN1 SpcIndirectDataContent object.
+ *
+ *  The SpcIndirectDataContent contains the digest and algorithm of the authenticode
+ *  hash generated for the PE. This function's role is to go down the ASN1 rabbit hole
+ *  and retreive this information so that the digest can be computed independently and
+ *  verified against the information contained in this signature.
+ *
+ *  @param  ASN1_STRING* asn1 The ASN1 string pointing to the SpcIndirectDataContent object.
+ *  @param  AuthenticodeDigest& digest The structure into which the digest information will be put.
+ *
+ *  @return Whether the ASN1 was parsed successfully.
+ */
+bool parse_spc_asn1(ASN1_STRING* asn1, AuthenticodeDigest& digest)
+{    
+    const unsigned char* asn1_data = asn1->data;
+    bytes buffer;
+
+    // Start at the SpcIndirectDataContent..
+    long size = asn1_read(&asn1_data, asn1->length, "SEQUENCE", "SpcIndirectDataContent");
+    if (size == 0) {
+        return false;
+    }
+    // Read the SpcAttributeTypeAndOptionalValue.
+    size = asn1_read(&asn1_data, asn1->length, "SEQUENCE", "SpcAttributeTypeAndOptionalValue");
+    if (size == 0) {
+        return false;
+    }
+    // Read SpcAttributeTypeAndOptionalValue->type
+    size = asn1_read(&asn1_data, asn1->length, "OBJECT", "type");
+    if (size == 0) {
+        return false;
+    }
+    // Assert that the type read has the expected OID.
+    buffer.assign(asn1_data, asn1_data + size);
+    if(OID_to_string(buffer) != SPC_PE_IMAGE_DATAOBJ)
+    {
+        PRINT_WARNING << "[plugin_authenticode] The SpcAttributeTypeAndOptionalValue has an invalid type!" << std::endl;
+        return false;
+    }
+    asn1_data += size; // Skip over the OID.
+    // Read SpcAttributeTypeAndOptionalValue->value (SpcPeImageData)
+    size = asn1_read(&asn1_data, asn1->length, "SEQUENCE", "SpcPeImageData");
+    if (size == 0) {
+        return false;
+    }
+    asn1_data += size; // Skip the structure.
+
+    // Read the DigestInfo.
+    size = asn1_read(&asn1_data, asn1->length, "SEQUENCE", "DigestInfo");
+    if (size == 0) {
+        return false;
+    }
+    // Read DigestInfo->AlgorithmIdentifier
+    size = asn1_read(&asn1_data, asn1->length, "SEQUENCE", "AlgorithmIdentifier");
+    if (size == 0) {
+        return false;
+    }
+    // Read DigestInfo->AlgorithmIdentifier->algorithm)
+    size = asn1_read(&asn1_data, asn1->length, "OBJECT", "algorithm");
+    if (size == 0) {
+        return false;
+    }
+    buffer.assign(asn1_data, asn1_data + size);
+    digest.algorithm = OID_to_string(buffer);
+    asn1_data += size;
+    // Read and skip DigestInfo->AlgorithmIdentifier->parameters
+    size = asn1_read(&asn1_data, asn1->length, "NULL", "parameters");
+    // Read the digest.
+    size = asn1_read(&asn1_data, asn1->length, "OCTET STRING", "digest");
+    if (size == 0) {
+        return false;
+    }
+    digest.digest.assign(asn1_data, asn1_data + size);
+
+    return true;
 }
 
 // ----------------------------------------------------------------------------
@@ -119,7 +344,13 @@ void add_certificate_information(pPKCS7 p, pResult res)
         PRINT_WARNING << "[plugin_authenticode] Could not obtain the certificate signers." << std::endl;
         return;
     }
-    
+
+    if (sk_X509_num(signers) != 1)
+    {
+        PRINT_WARNING << "[plugin_authenticode] Authenticode signature should contain a single SignerInfo structure, but " 
+                      << sk_X509_num(signers) << " were found." << std::endl;
+    }
+
     for (int i = 0 ; i < sk_X509_num(signers) ; ++i)
     {
         // X509_NAMEs don't need to be freed.
@@ -136,6 +367,15 @@ void add_certificate_information(pPKCS7 p, pResult res)
 
 // ----------------------------------------------------------------------------
 
+bool verify_signature(const mana::PE& pe, pPKCS7 signature, pResult res)
+{
+    //pBIO in(BIO_new(BIO_s_mem()), BIO_free);
+    //BIO_puts(in.get(), "YOLOCRYPTO!");
+    pBIO out(BIO_new(BIO_s_mem()), BIO_free);
+    int verification = PKCS7_verify(signature.get(), nullptr, nullptr, NULL, out.get(), PKCS7_BINARY);
+    //std::cout << "Verification result: " << verification << std::endl;
+}
+
 /**
  *  @brief  This plugin verifies the authenticode signature of a PE file.
  *
@@ -150,14 +390,20 @@ void add_certificate_information(pPKCS7 p, pResult res)
 class OpenSSLAuthenticodePlugin : public IPlugin
 {
     int get_api_version() const override { return 1; }
+    
+    // ----------------------------------------------------------------------------
 
     pString get_id() const override {
         return boost::make_shared<std::string>("authenticode");
     }
+    
+    // ----------------------------------------------------------------------------
 
     pString get_description() const override {
         return boost::make_shared<std::string>("Checks if the digital signature of the PE is valid.");
     }
+    
+    // ----------------------------------------------------------------------------
 
     pResult analyze(const mana::PE& pe) override
     {
@@ -184,17 +430,23 @@ class OpenSSLAuthenticodePlugin : public IPlugin
                 continue;
             }
             
+            // Have OpenSSL parse the certificate and check that it isn't malformed.
             pPKCS7 p(d2i_PKCS7_bio(bio.get(), NULL), PKCS7_free);
-            if (p == nullptr)
+            if (!check_pkcs_sanity(p)) {
+                continue;
+            }
+
+            AuthenticodeDigest digest;
+            if (!parse_spc_asn1(p->d.sign->contents->d.other->value.asn1_string, digest))
             {
-                PRINT_WARNING << "[plugin_authenticode] Error reading the PKCS7 certificate." << std::endl;
+                PRINT_WARNING << "[plugin_authenticode] Could not read the digest information." << std::endl;
                 continue;
             }
             
-            // The PKCS7 certificate has been loaded successfully. Perform verifications.
+            // The PKCS7 certificate has been parsed successfully. Start the analysis.
             add_certificate_information(p, res);
+            verify_signature(pe, p, res);
             res->set_summary("The PE is digitally signed.");
-            
         }
         
         return res;
